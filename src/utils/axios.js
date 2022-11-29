371616ecfc2ebe3e0b30ae95a8448b27cd50ba91
Leon Acero <acero@hotmail.com>
2022-11-28 18:32:47 -0600

    actualizacion de colores

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 		
-  export const BASE_URL = 'http://127.0.0.1:8000';
-  // export const BASE_URL = 'https://eljuanjo-dulces.herokuapp.com';
+  // export const BASE_URL = 'http://127.0.0.1:8000';
+  export const BASE_URL = 'https://eljuanjo-dulces.herokuapp.com';
 
   export default axios.create ({
     baseURL: BASE_URL,
